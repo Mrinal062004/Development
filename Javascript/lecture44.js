7759eaa32432df6fc401aa0a4872609a1ad5532f
mrinal062004 <tiwari.mrinal2004@gmail.com>
2026-08-09 16:53:48 +0530

    Study sync Project started

--- Javascript/lecture44.js
+++ Javascript/lecture44.js
@@ -1,20 +1,59 @@
-// // Arrays in Javascript
-// let obj={
-//     name: "Love",
-//     age:25,
-//     weight:85,
-//     height:"6ft 1in",
-//     greet: function(){
-//         console.log(
-//             "hello jee "
-//         )
+// // // Arrays in Javascript
+// // let obj={
+// //     name: "Love",
+// //     age:25,
+// //     weight:85,
+// //     height:"6ft 1in",
+// //     greet: function(){
+// //         console.log(
+// //             "hello jee "
+// //         )
+// //     }
+// // };
+// // console.log(obj)
+// // obj.greet()
+// //arrays is list of items
+// let arr=[1,2,3,4,5,6,7];
+// let brr =new Array("love",1,true);
+// brr.push("babbar")
+// brr.push(27)
+// brr.push(34)
+// brr.push(40)
+// brr.pop()
+// brr.shift()
+// brr.unshift("Halwa")
+// let ans=brr.slice(2,4)
+
+// //  Built in Method in Array
+// //splice method used to remove 
+// brr.splice(1,2,"ballu")
+// console.log(arr[0],brr,ans);
+// let ansArray=arr.map((number)=>{
+//     return number*number;
+// }
+
+// )
+// console.log(ansArray)
+
+// // filter
+// let ad=[10,20,30,11,21,44,32];
+// let ansArra=arr.filter((number) => {
+//     if(number%2==0){
+//         return true;
 //     }
-// };
-// console.log(obj)
-// obj.greet()
-//arrays is list of items
-let arr=[1,2,3,4,5,6,7];
-let brr =new Array("love",1,true);
-brr.push("babbar")
-console.log(arr[0],brr);
-//  Built in Method in Array
+//     else{
+//         return false;
+//     }
+
+// })
+// console.log(ansArra)
+let arr=[10,20,30,40,50,13,15,23];
+arr.sort((a,b)=>b-a);
+// let ans=arr.reduce((acc,curr)=>{
+//     return acc+curr;
+// },0
+// );
+// console.log(ans);
+console.log(arr)
+console
+
